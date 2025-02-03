81654ed3855f789cca3349ee611de8531a8708da
Rodinchuk <rodandser@gmail.com>
2025-02-03 21:31:39 +0200

    fix functionality add opps and cart

--- src/app/components/EventAdvantages.tsx
+++ src/app/components/EventAdvantages.tsx
@@ -1,6 +1,7 @@
 "use client";
 import "./EventAdvantages.css";
 import React, { useState } from "react";
+import Image from "next/image";
 
 interface ExpandableCardProps {
   title: string;
@@ -18,11 +19,11 @@ const ExpandableCard: React.FC<ExpandableCardProps> = ({ title, description, ico
     >
       <div className="card-header">
         <div className="card-icon">
-          <img src={icon} alt="icon" />
+          <Image src={icon} alt="icon" width={50} height={50} />
         </div>
         <h3 className="card-title">{title}</h3>
         <div className={`card-toggle ${isExpanded ? "rotate-180" : ""}`}>
-          <img src="./images/toddle.png" alt="toggle" />
+          <Image src="/images/toddle.png" alt="toggle" width={24} height={24} />
         </div>
       </div>
       {isExpanded && <p className="card-description">{description}</p>}
@@ -36,39 +37,37 @@ const EventAdvantages: React.FC = () => {
       title: "Знайдете перспективних працівників",
       description:
         "Ви зможете особисто поспілкуватися зі студентами різних спеціальностей, оцінити їх потенціал та сформувати базу перспективних кандидатів на актуальні для Вас вакансії.",
-      icon: "./images/adv_perspective.png",
+      icon: "/images/adv_perspective.png",
     },
     {
       title: "Зміцните свій бренд як роботодавця",
       description:
         "Ярмарок кар'єри – це чудова можливість представити свою компанію студентам, сформувати уявлення про Вашу корпоративну культуру, цінності та можливості для розвитку кар'єри.",
-      icon: "./images/adv_brand.png",
+      icon: "/images/adv_brand.png",
     },
     {
       title: "Дізнаєтесь очікування молоді щодо ринку праці",
       description:
         "Участь у різноманітних воркшопах та панельних дискусіях, допоможе Вам краще зрозуміти потреби сучасної молоді. Це сприятиме ефективній співпраці зі студентами у майбутньому.",
-      icon: "./images/adv_expectations.png",
+      icon: "/images/adv_expectations.png",
     },
     {
       title: "Розширите мережу контактів",
       description:
         "Наш захід – це платформа для знайомств з представниками інших компаній та встановлення нових партнерських відносин.",
-      icon: "./images/adv_web.png",
+      icon: "/images/adv_web.png",
     },
-    
     {
       title: "Отримаєте можливості для просування Вашого бренду",
       description:
         "Інтерактивні активності, такі як Share and Win, Treasure Hunt та інші, дозволять залучити аудиторію до взаємодії з Вашим брендом у соцмережах та збільшать впізнаваність серед молоді.",
-      icon: "./images/adv_promo.png",
+      icon: "/images/adv_promo.png",
     },
-   
     {
       title: "Інвестуєте у майбутнє та амбітну молодь",
       description:
         "Це можливість допомогти реалізувати кар’єрний потенціал студентів, а також поділитись знаннями та досвідом Вашої команди на активностях нашого ярмарку.",
-      icon: "./images/adv_invest.png",
+      icon: "/images/adv_invest.png",
     },
   ];
 
@@ -81,7 +80,7 @@ const EventAdvantages: React.FC = () => {
             <ExpandableCard key={index} {...card} />
           ))}
         </div>
-        <img src="./images/lupkaZalupka.png" className="overlay-image" alt="overlay" />
+        <Image src="/images/lupkaZalupka.png" className="overlay-image" alt="overlay" width={500} height={500} />
       </div>
     </div>
   );
