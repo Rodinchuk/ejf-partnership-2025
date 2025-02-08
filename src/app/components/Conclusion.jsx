7fd12c5c1a027baff7ad35f8258e56b21cb64e59
Rodinchuk <rodandser@gmail.com>
2025-02-08 03:35:48 +0200

    added bubbles

--- src/app/components/Conclusion.jsx
+++ src/app/components/Conclusion.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./Conclusion.module.css";
 import bg from '../../../public/images/bg-hero.png'
-import Link from "next/link";
 
 const Conclusion = () => {
   return (
-    <section className={styles.conclusion}>
+    <div className={styles.conclusion}>
+      <div className={styles.conclusioncontainer}>
       <Image src={bg} alt="img" className={styles.conclusionWave}></Image>
       <div className={styles.conclusioncontent}>
       <h2 className={styles.title}>Інвестуй у майбутнє! </h2>
@@ -13,11 +13,11 @@ const Conclusion = () => {
         Приєднуйтесь до 17-го Інженерного Ярмарку Кар'єри, щоб розвивати молодь та бізнес разом!
       </p>
      <a href="#partnership"><button className={styles.conclusionbutton}>Співпрацювати</button></a> 
-      </div>
+      </div></div>
       
       <img src="./images/down-balls-big.png" className={styles.downballsbig}></img>
       <img src="./images/down-balls.png" className={styles.downballs}></img>
-    </section>
+    </div>
   );
 };
 
